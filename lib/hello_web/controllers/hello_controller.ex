db862d497e89b90b55364a61c20a269a3d652b94
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2022-03-17 16:02:42 +0300

    first commit

---
+++ lib/hello_web/controllers/hello_controller.ex
@@ -0,0 +1,14 @@
+defmodule HelloWeb.HelloController do
+  use HelloWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    # render(conn, "show.html", messenger: messenger)
+    # text(conn, "From messenger #{messenger}")
+    # json(conn, %{id: messenger})
+    send_resp(conn, 201, "")
+  end
+end
